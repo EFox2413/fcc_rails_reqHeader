53a5a0358ff8e49fc6f6ee374782859cd4dd989b
gitolite <EFox2413@gmail.com>
2016-06-22 17:20:51 -0700

    removed unecessary components from trunk

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,12 @@
 class WelcomeController < ApplicationController
-  def index
-  end
+    def getHeader
+        ip = request.remote_ip
+        lang = request.env["HTTP_ACCEPT_LANGUAGE"]
+        software = request.env["HTTP_USER_AGENT"]
+
+        lang = lang[/(.*),/, 1]
+        software = software[/\((.*)\)/, 1].split(')')[0]
+
+        render json: { ipaddress: ip, language: lang, software: software}
+    end
 end
